452ba165962f651eca80add02d1fe68bf59c0b6b
Spappz <34202141+Spappz@users.noreply.github.com>
2021-09-23 23:49:03 +0100

    Create repo

---
+++ File Dater.ps1
@@ -0,0 +1,34 @@
+﻿<#
+ Outputs a tab-separated text file with a list of filenames (relative
+ to $startpath) and creation timestamps.
+
+ The resulting file can get quite big.
+
+
+
+ $startPath   (array of strings)
+    Absolute paths to your desired start folder. 
+
+ $output   (string)
+    Absolute path to the desired output text file.
+
+
+
+ Spappz 2021
+ 
+ MIT License
+ No warranty or guarantee provided
+ All rights reserved
+#> 
+
+$startPath = ( "C:\Users", "D:\" )
+$output = "D:\Desktop\file.txt"
+
+Get-ChildItem $startPath -Recurse -Attributes "!D" | ForEach-Object {
+    $_.FullName
+    (
+        (($_.FullName | Resolve-Path -Relative) -replace "\\", "/") `
+        + "`t" `
+        + ($_.CreationTime) `
+    ) | Out-File $output -Append
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # File Dater
- Creates a log of files and their creation dates
+ Creates a log of files and their creation dates.
+
+ This code outputs a tab-separated text file with a list of filenames (relative to `$startpath`) and the creation timestamp for each. Bear in mind the resulting file can get quite big.
+
+## Settings
+##### `$startPath` _array of strings_
+Absolute paths to your desired start folder. 
+
+##### `$output` _string_
+Absolute path to the desired output text file.
